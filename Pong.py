14b470ac60dfa73050147dd6ef97a7b63908878b
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-01-05 20:56:01 +0100

    Created ball

--- Pong.py
+++ Pong.py
@@ -12,13 +12,27 @@ paddle_A = turtle.Turtle()
 paddle_A.speed(0)
 paddle_A.shape("square")
 paddle_A.color("white")
+paddle_A.shapesize(stretch_wid=5, stretch_len=1)
 paddle_A.penup()
 paddle_A.goto(-350,0)
 
 #Paddle B
 paddle_B = turtle.Turtle()
+paddle_B.speed(0)
+paddle_B.shape("square")
+paddle_B.color("white")
+paddle_A.shapesize(stretch_wid=5, stretch_len=1)
+paddle_B.penup()
+paddle_B.goto(350,0)
 
-#Square
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
 
 #Main game loop
 while True:
